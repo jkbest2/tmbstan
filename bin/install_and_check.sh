0a1b667d098858d29d6f3b153eb92475630c44b7
Kasper Kristensen <kaskr@dtu.dk>
2018-11-07 08:34:24 +0100

    2nd try

--- bin/install_and_check.sh
+++ bin/install_and_check.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-Rscript -e 'pkg <- c("rstan", "TMB"); if(!all(pkg%in%installed.packages()))install.packages(pkg)'
+if [ ! -f ~/.R/Makevars ]; then
+    mkdir -p ~/.R
+    echo "CXX14 = g++" > ~/.R/Makevars
+    echo "CXX14FLAGS = -O3" >> ~/.R/Makevars
+fi
 
 make build
 make check
--- bin/install_dependencies.sh
+++ bin/install_dependencies.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+if [ ! -f ~/.R/Makevars ]; then
+    mkdir -p ~/.R
+    echo "CXX14 = g++" > ~/.R/Makevars
+    echo "CXX14FLAGS = -O3" >> ~/.R/Makevars
+fi
+
 Rscript -e 'pkg <- c("rstan", "TMB"); if(!all(pkg%in%installed.packages()))install.packages(pkg)'
