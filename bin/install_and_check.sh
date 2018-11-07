ff3ac479b2c3b2059d5b07bdc131c669491eaac4
Kasper Kristensen <kaskr@dtu.dk>
2018-11-07 08:15:26 +0100

    SHOULD NOT BE NEEDED

--- bin/install_and_check.sh
+++ bin/install_and_check.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
+Rscript -e 'pkg <- c("rstan", "TMB"); if(!all(pkg%in%installed.packages()))install.packages(pkg)'
+
 make build
 make check
 make install
